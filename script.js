755c148220ca6a7a34a56d8646e1f2b4b68b5d26
Jagmohan-cmd <141887538+Jagmohan-cmd@users.noreply.github.com>
2023-10-27 10:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+  let target = document.getElementById("sampleTable");
+   let tr = document.createElement("tr");
+   let td1 = "<td>New Cell1</td>";
+   let td2 = "<td>New Cell2</td>";
+   tr.insertAdjacentHTML("afterbegin" ,td1);
+   tr.insertAdjacentHTML("beforeend" , td2);
+   target.insertBefore(tr , target.childNodes[0]);
   
 }
